feb3db85999acd76d6d18cee14a6eadad6fea075
Khoi Lai <k@khoi.io>
2017-01-30 14:54:06 +0700

    Remove `cleanup whitespace` scheme

--- SwiftlyTests/SwiftlyTests.swift
+++ SwiftlyTests/SwiftlyTests.swift
@@ -1,35 +1,35 @@
 //
 //  SwiftlyTests.swift
-//  SwiftlyTests
+//  SwiftlyExt
 //
-//  Created by Khoi Lai on 26/09/2016.
-//  Copyright © 2016 Khoi Lai. All rights reserved.
+//  Created by Khoi Lai on 1/30/17.
+//  Copyright © 2017 Khoi Lai. All rights reserved.
 //
 
 import XCTest
 
 class SwiftlyTests: XCTestCase {
-
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
